b66cc35d8dad072ce1a358cb14f5a3f7b68beb74
david <a0983511138@gmail.com>
2022-07-18 16:26:00 +0800

    try to complete q_new and q_free

--- queue.c
+++ queue.c
@@ -14,11 +14,15 @@
 /* Create an empty queue */
 struct list_head *q_new()
 {
-    return NULL;
+    struct list_head *l = malloc(sizeof(struct list_head));
+    return l;
 }
 
 /* Free all storage used by queue */
-void q_free(struct list_head *l) {}
+void q_free(struct list_head *l)
+{
+    free(l);
+}
 
 /* Insert an element at head of queue */
 bool q_insert_head(struct list_head *head, char *s)
